11911abed53d171b29bb5fc005b5d21602475a32
walksydn <sydwal5434@gmail.com>
2016-01-23 15:14:39 -0500

    Ultrasonic Code
    Able to read distances and go to a distance

---
+++ UltraSonicSensor/src/org/usfirst/frc/team20/robot/Robot.java
@@ -0,0 +1,139 @@
+
+package org.usfirst.frc.team20.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    CANTalon fr;
+	CANTalon br;
+	CANTalon fl;
+	CANTalon bl;
+	Joystick joy;
+	AnalogInput sensor;
+	double vcc; 
+	AnalogInput voltage;
+	double inches;
+	double calculatedInches;
+	Sensors s = new Sensors();
+	
+	public void robotInit() {
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);
+        fr = new CANTalon(9);
+    	br = new CANTalon(2);
+    	fl = new CANTalon(10);
+    	bl = new CANTalon(1);
+    	joy = new Joystick(0);
+    	sensor = new AnalogInput(3);
+    	voltage = new AnalogInput(2);
+    	
+    	
+    }
+    
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    	
+   	
+    /**
+     * This function is called periodically during autonomous
+     */
+	public void autonomousInit(){
+		calculatedInches = sensor.getAverageVoltage()/(vcc/512);
+	}	
+    public void autonomousPeriodic() {
+//    	double numberInches;
+//    	numberInches = getSensorInches();
+//    	System.out.println(numberInches);
+//    	if(numberInches > 13){
+//    		fr.set(-.5);
+//    		br.set(-.5);
+//    		fl.set(0);
+//    		bl.set(0);
+//    		
+//    	}
+//    	else if(numberInches < 11){
+//    		fr.set(0);
+//    		br.set(0);
+//    		fl.set(.5);
+//    		bl.set(.5);
+//    	}else{
+//    		fr.set(-.25);
+//        	br.set(-.25);	
+//        	fl.set(.25);
+//        	bl.set(.25);	
+//    	}
+    
+    }
+    
+
+    /**
+     * This function is called periodically during operator control
+     */
+    double inchesGoal = 12;
+
+    public void teleopPeriodic() {
+    	System.out.println("Inches Goal:" + inchesGoal);
+    	if(joy.getRawButton(5)){
+    		inchesGoal += 6;  		
+    	}
+    	if(joy.getRawButton(4)){
+    		inchesGoal -= 6;
+    		if(inchesGoal < 12){
+    			inchesGoal = 12;
+    		}
+    	}
+    	s.goToDistance(inchesGoal);
+    }
+    
+    
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	System.out.println("Voltage:" + voltage.getVoltage());
+    	if(getSensorInches() > 12){
+    		fr.set(0);
+    		fl.set(0);
+    		br.set(0);
+    		bl.set(0);
+    	}else{
+    		fr.set(-.2);
+    		fl.set(.2);
+    		br.set(-.2);
+    		bl.set(.2);
+    		
+    	}
+    }
+    
+}
---
+++ UltraSonicSensor/src/org/usfirst/frc/team20/robot/Sensors.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team20.robot;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Sensors {
+	private double vcc;
+	private double inches;
+	private AnalogInput sensor;
+	private AnalogInput voltage;
+	private double inchesFromWall;
+	
+	public Sensors(){
+		sensor = new AnalogInput(3);
+    	voltage = new AnalogInput(2);
+	}
+	public double getDistance(){
+		vcc = voltage.getVoltage();
+        inches = sensor.getAverageVoltage()/(vcc/512);		//takes sensor input, compares against voltage, and convert to inches
+    	return inches;
+	}
+	public void goToDistance(double targetInches){
+		inchesFromWall = getDistance();
+		if(inchesFromWall < targetInches + 3 && inchesFromWall > targetInches - 3){	
+//    		fr.set(0);					To do: insert code for drive train stopping
+//    		br.set(0);
+//    		fl.set(0);
+//    		bl.set(0);
+    	}else if(inchesFromWall < targetInches + 3){
+//    		fr.set(.15);				To do: insert code for drive train forward
+//    		br.set(.15);
+//    		fl.set(-.15);
+//    		bl.set(-.15);
+    	}else if(inchesFromWall > targetInches - 3){
+//    		fr.set(-.15);				To do: insert code for drive train backwards
+//    		br.set(-.15);
+//    		fl.set(.15);
+//    		bl.set(.15);
+    	}
+	}
+}
